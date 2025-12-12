ce152c6ad498b8de27ebcc14773303064f34021b
Amina <minassouane@gmail.com>
2025-12-12 14:39:18 +0100

    Change rendered code

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,6 +1,14 @@
 function Greeting() {
   return (
-    <h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>
+    <div>
+      <h1>Test title</h1>
+      <svg>
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+        <input type="text" />
+      </form>
+    </div>
   );
 }
 
